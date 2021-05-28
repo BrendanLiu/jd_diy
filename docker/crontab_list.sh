e76896ea65e76700b46a2e3d3813607b0c995118
Brendan <brendan.b.liu@gmail.com>
2021-05-28 15:10:52 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -28,6 +28,9 @@
 #家电星推官 活动时间：2021年5月27日 00:00:00-2021年6月18日 23:59:59
 0 0 * * * node /scripts/jd_xtg.js >> /scripts/logs/jd_xtg.log 2>&1
 
+#金榜创造营 活动时间：2021年5月21日-2021年12月31日
+0 1,22 * * * node /scripts/jd_gold_creator.js >> /scripts/logs/jd_gold_creator.log 2>&1
+
 ##############长期活动##############
 # 签到
 5,10 0,7,9,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
